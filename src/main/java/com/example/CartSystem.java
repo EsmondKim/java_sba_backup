2fa72832313b84bdc3e2e2db27304d8ac77c3df9
EsmondKim <ekelectronicmail@gmail.com>
2022-03-15 00:04:55 -0500

    12:04 am Tuesday

--- src/main/java/com/example/CartSystem.java
+++ src/main/java/com/example/CartSystem.java
@@ -17,10 +17,13 @@ public class CartSystem extends TheSystem {
         Iterator iCIterator = itemCollection.entrySet().iterator();
         while (iCIterator.hasNext()) {
             Map.Entry mapElement = (Map.Entry)iCIterator.next();
-            if (itemCollection.get(mapElement.getKey()).getQuantity() > 1) {
-                System.out.printf( "%-20s %-20s %-10s %-10d\n", itemCollection.get(mapElement.getKey()).getItemName(), itemCollection.get(mapElement.getKey()).getItemDesc(), itemCollection.get(mapElement.getKey()).getItemPrice(), itemCollection.get(mapElement.getKey()).getQuantity() );
+            if (itemCollection.get(mapElement.getKey()).getQuantity() > 0) {
+                Double itemSubtotal = itemCollection.get(mapElement.getKey()).getItemPrice() * itemCollection.get(mapElement.getKey()).getQuantity();
+                System.out.printf( "%-20s %-20s %-10s %-10d %10s\n", itemCollection.get(mapElement.getKey()).getItemName(), itemCollection.get(mapElement.getKey()).getItemDesc(), itemCollection.get(mapElement.getKey()).getItemPrice(), itemCollection.get(mapElement.getKey()).getQuantity(), itemSubtotal );
+
             }//nested If
         }//while iCIterator
+
             //This Lambda forEach worked, but you probably don't need to use it.
 //          itemCollection.forEach((k,v) -> System.out.printf("k: %-10s v: %-10s %-10s %-10d\n", k, v.getItemDesc(), v.getItemPrice(), v.getAvailableQuantity()));
     }//display()
